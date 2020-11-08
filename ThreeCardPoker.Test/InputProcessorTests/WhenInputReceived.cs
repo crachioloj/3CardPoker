ce842ea08cf79247bc59b7f7f8bf742815e7fd7e
Joseph Crachiolo <crachioloj@gmail.com>
2020-11-08 17:32:40 -0500

    More input tests.

--- ThreeCardPoker.Test/InputProcessorTests/WhenInputReceived.cs
+++ ThreeCardPoker.Test/InputProcessorTests/WhenInputReceived.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ThreeCardPoker;
 
-using static ThreeCardPoker.InputProcessor;
-
 namespace ThreeCardPoker.Test.InputProcessorTests
 {
     [TestClass]
@@ -50,6 +48,51 @@ namespace ThreeCardPoker.Test.InputProcessorTests
         public void AndPlayerCountDoesNotMatchPlayerRowsThenExceptionIsThrown(string input)
         {
             var _ = InputProcessor.GetGameInfoFromStringInput(input);
+        }
+
+        [TestMethod]
+        [DataRow(
+@"3
+0 2c As 4d
+1    Kd 5h 6c
+2 Jc Jd   9s  ", 3)]
+        [DataRow(
+@"
+2
+
+    1  Kd 5h 6c                   
+
+ 2 Jc Jd   9s  ", 2)]
+        public void AndInputIsValidWithExtraWhiteSpaceThenCorrectGameInfoCreated(string input, int playerCount)
+        {
+            var info = InputProcessor.GetGameInfoFromStringInput(input);
+            Assert.AreEqual(playerCount, info.PlayerCount);
+            foreach (var player in info.Players)
+            {
+                Assert.AreEqual(3, player.Cards.Count());
+            }
+        }
+
+        [TestMethod]
+        [DataRow(
+@"3
+0 2c As 4d
+1 Kd 5h 6c
+2 Jc Jd 9s", 3)]
+        [DataRow(
+@"4
+0 Qc Kc 4s
+1 Ah 2c Js
+2 3h 9h Th
+3 Tc 9c 3c", 4)]
+        public void AndInputIsThenCorrectGameInfoCreated(string input, int playerCount)
+        {
+            var info = InputProcessor.GetGameInfoFromStringInput(input);
+            Assert.AreEqual(playerCount, info.PlayerCount);
+            foreach (var player in info.Players)
+            {
+                Assert.AreEqual(3, player.Cards.Count());
+            }
         }
     }
 }
